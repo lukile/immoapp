e71877a226b6646e103b1bfe6b4ccb94929ddde2
lukile <lucile.1988.ls@gmail.com>
2015-08-10 22:34:21 +0200

    Ajout d'un index.html avec header et menu

---
+++ src/main/java/website/ImmoappApplication.java
@@ -0,0 +1,12 @@
+package website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmoappApplication.class, args);
+    }
+}
